f1095138d6a5979b674de04028b82e2642c009c2
Matthew Thompson <chatrat12@gmail.com>
2021-01-18 12:07:29 -0500

    Playing with audio

--- Assets/Scripts/Interactions/ResourceSource.cs
+++ Assets/Scripts/Interactions/ResourceSource.cs
@@ -5,11 +5,19 @@ public class ResourceSource : InteractableObject
     [SerializeField] ItemStack _item;
     [SerializeField] Transform _model;
     [SerializeField] Transform _depletedOverride;
+    [SerializeField] AudioClip _harvestSound;
+
+    private AudioSource _source;
 
     public override string InteractMessage => base.InteractMessage;
     public bool Depleted { get; private set; }
 
     public override bool CanInteract(Player player) => !Depleted;
+
+    private void Awake()
+    {
+        CreateAudioSource();
+    }
 
     public override void Interact(Player player)
     {
@@ -19,5 +27,16 @@ public class ResourceSource : InteractableObject
         _model.gameObject.SetActive(false);
         if (_depletedOverride != null)
             _depletedOverride.gameObject.SetActive(true);
+
+        if (_source != null)
+            _source.Play();
+    }
+
+
+    private void CreateAudioSource()
+    {
+        if (_harvestSound == null) return;
+        _source = gameObject.AddComponent<AudioSource>();
+        _source.clip = _harvestSound;
     }
 }
